56819a9b22d5a07ea5dae957f5f81ac57abae1d7
Student <Student@B2-411-11>
2024-04-03 13:35:06 +0400

    min

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            var login = LoginBox.Text;
+            var password = PasswordBox.Text;
+            var context = new AppDbContext();
+
+            var user = context.Users.SingleOrDefault(x => x.login == login && x.password == password);
+            if (user is null)
+            {
+                MessageBox.Show("Неправильный логин или пароль!");
+            }
+            MessageBox.Show("Вы успешно вошли в аккаунт!");
+        }
+        private void DeleteUser()
+        {
+            var userId = 1;
+            var context = new AppDbContext();
+            var user = context.Users.Find(userId);
+
+            context.Users.Remove(user);
+            context.SaveChanges();
+        }
+
+        private void UpdateUser()
+        {
+            var userId = 1;
+            var context = new AppDbContext();
+            var user = context.Users.Find(userId);
+
+            user.login = "admin1";
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+        public List<User> Transports { get; set; }
+    }
+
+    public class Transport
+    {
+        public int Id { get; set; }
+        public User Owner { get; set; }
+        public string Identifier { get; set; }
+        public DbSet<Transport> Transports { get; set; }
+    }
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=registr;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void RegBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var login   = loginbox.Text;
+
+            var pass    = PasswordBox.Text;
+
+            var context = new AppDbContext();
+
+            var user_exists = context.Users.FirstOrDefault(x => x.login == login);
+            if (user_exists is not null)
+            {
+                MessageBox.Show("Такой пользователь уже в клубе крутяшек");
+                return;
+            }
+            var user = new User { login = login, password = pass };
+            context.Users.Add(user);
+            context.SaveChanges();
+            MessageBox.Show("welcome to the club, buddy");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
