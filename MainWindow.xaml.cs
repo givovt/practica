489d2b7202e58f07626efe916bd2cd746f87bfd1
Student <Student@B2-411-11>
2024-04-04 11:01:38 +0400

    practica day 3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
 using System.Security.Cryptography.Xml;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,38 +30,63 @@ namespace WpfApp2
             InitializeComponent();
         }
 
+        int mistake = 0;
         private void login_Click(object sender, RoutedEventArgs e)
         {
             var login = LoginBox.Text;
-            var password = PasswordBox.Text;
+            var password = PasswordBox.Password;
             var context = new AppDbContext();
 
-            var user = context.Users.SingleOrDefault(x => x.login == login && x.password == password);
+            var user = context.Users.SingleOrDefault(x => x.login == login && x.password == password || x.email == login && x.password == password);
             if (user is null)
             {
-                MessageBox.Show("Неправильный логин или пароль!");
+                result_text.Text = "Неправильный логин,эмейл или пароль!";
+                mistake++;
+                if (mistake % 3 == 0)
+                {
+                    MessageBox.Show("Вы 3 раза ошиблись, вы задержины полицией кринжа на 15 секунд");
+                    Thread.Sleep(15000);
+                }
             }
-            MessageBox.Show("Вы успешно вошли в аккаунт!");
-        }
-        private void DeleteUser()
-        {
-            var userId = 1;
-            var context = new AppDbContext();
-            var user = context.Users.Find(userId);
-
-            context.Users.Remove(user);
-            context.SaveChanges();
+            else
+            {
+                result_text.Text = "Вы успешно вошли в аккаунт!";
+                result form = new result();
+                form.Title = $"Добро пожаловать {LoginBox.Text}";
+                form.Show();
+                this.Close();
+            }
         }
 
-        private void UpdateUser()
+        private void switch_Click(object sender, RoutedEventArgs e)
         {
-            var userId = 1;
-            var context = new AppDbContext();
-            var user = context.Users.Find(userId);
+            Window1 form = new Window1();
+            form.Show();
+            this.Close();
+        }
 
-            user.login = "admin1";
-
-            context.SaveChanges();
+        int chet = 0;
+        private void show_off_password_Click(object sender, RoutedEventArgs e)
+        {
+            chet++;
+            if (chet % 2 == 0)
+            {
+                PasswordBox.Password = passres.Text;
+                passres.Visibility = Visibility.Hidden;
+                passres.IsEnabled = false;
+                PasswordBox.Visibility = Visibility.Visible;
+                PasswordBox.IsEnabled = true;
+                eye.Source = new BitmapImage(new Uri("C:\\Минаев\\WpfApp2\\res\\eyes1.png"));
+            }
+            else
+            {
+                passres.Text = PasswordBox.Password;
+                passres.Visibility = Visibility.Visible;
+                passres.IsEnabled = true;
+                PasswordBox.Visibility = Visibility.Hidden;
+                PasswordBox.IsEnabled = false;
+                eye.Source = new BitmapImage(new Uri("C:\\Минаев\\WpfApp2\\res\\eyes2.png"));
+            }
         }
     }
 }
--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Azure.Core.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
 using System.Windows.Shapes;
 
 namespace WpfApp2
@@ -21,15 +23,7 @@ namespace WpfApp2
         public int Id { get; set; }
         public string login { get; set; }
         public string password { get; set; }
-        public List<User> Transports { get; set; }
-    }
-
-    public class Transport
-    {
-        public int Id { get; set; }
-        public User Owner { get; set; }
-        public string Identifier { get; set; }
-        public DbSet<Transport> Transports { get; set; }
+        public string email { get; set; }
     }
     public class AppDbContext : DbContext
     {
@@ -37,7 +31,7 @@ namespace WpfApp2
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=registr;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MinaevDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
         }
     }
     public partial class Window1 : Window
@@ -47,29 +41,114 @@ namespace WpfApp2
             InitializeComponent();
         }
 
+
         private void RegBtn_Click(object sender, RoutedEventArgs e)
         {
             var login   = loginbox.Text;
 
-            var pass    = PasswordBox.Text;
+            var pass    = PasswordBox.Password;
+
+            var emaill = email_box.Text;
 
             var context = new AppDbContext();
 
             var user_exists = context.Users.FirstOrDefault(x => x.login == login);
             if (user_exists is not null)
             {
-                MessageBox.Show("Такой пользователь уже в клубе крутяшек");
+                result_reg_text.Text = "Такой пользователь уже в клубе крутяшек";
+                n1.Visibility = Visibility.Visible;
+                n2.Visibility = Visibility.Hidden;
+                n3.Visibility = Visibility.Hidden;
+                n4.Visibility = Visibility.Hidden;
                 return;
             }
-            var user = new User { login = login, password = pass };
-            context.Users.Add(user);
-            context.SaveChanges();
-            MessageBox.Show("welcome to the club, buddy");
+
+            char[] charr = {'!','@','#','$','%','^','&','*','(',')'};
+            int sum = 0;
+            int sum2 = 0;
+
+            var email_exists = context.Users.FirstOrDefault(x => x.email == emaill);
+
+            if(email_exists is not null)
+            {
+                result_reg_text.Text = "Человек с таким адресом уже зарегестрирован";
+                n1.Visibility = Visibility.Hidden;
+                n2.Visibility = Visibility.Visible;
+                n3.Visibility = Visibility.Hidden;
+                n4.Visibility = Visibility.Hidden;
+                return;
+            }
+
+            for (int i = 0; emaill.Length > i; i++)
+            {
+                if (emaill[i] == charr[1])
+                {
+                    sum2++;
+                }
+            }
+
+            if(sum2 < 1)
+            {
+                result_reg_text.Text = "Адрес не коректен";
+                n1.Visibility = Visibility.Hidden;
+                n2.Visibility = Visibility.Visible;
+                n3.Visibility = Visibility.Hidden;
+                n4.Visibility = Visibility.Hidden;
+                return;
+            }
+
+            for (int i = 0; PasswordBox.Password.Length > i; i++)
+            {
+                if (pass.Contains(charr[i]) == true)
+                {
+                    sum++;
+                }
+            }
+
+            if (sum < 1)
+            {
+                result_reg_text.Text = "В пароле должны присутсвовать символы";
+                n1.Visibility = Visibility.Hidden;
+                n2.Visibility = Visibility.Hidden;
+                n3.Visibility = Visibility.Visible;
+                n4.Visibility = Visibility.Hidden;
+                return;
+            }
+
+            if (pass.Length < 6)
+            {
+                result_reg_text.Text = "Пароль слишком маленький";
+                n1.Visibility = Visibility.Hidden;
+                n2.Visibility = Visibility.Hidden;
+                n3.Visibility = Visibility.Visible;
+                n4.Visibility = Visibility.Hidden;
+                return;
+            }
+            if (PasswordBox.Password != Password2.Password)
+            {
+                n4.Visibility = Visibility.Visible;
+                result_reg_text.Text = "Вы не подтвердили пароль";
+                n1.Visibility = Visibility.Hidden;
+                n2.Visibility = Visibility.Hidden;
+                n3.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                var user = new User { login = login, password = pass, email = emaill};
+                context.Users.Add(user);
+                context.SaveChanges();
+                result_reg_text.Text = "welcome to the club, buddy";
+                MainWindow form = new MainWindow();
+                form.Show();
+                this.Close();
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            MainWindow form = new MainWindow();
+            form.Show();
+            this.Close();
         }
     }
 }
